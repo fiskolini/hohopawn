23ebc7f2dd585db2ab1638852449d911788249fc
Francisco Carvalho <fiskolini@gmail.com>
2019-12-13 09:04:20 +0000

    update random sentences

--- src/scripts/spam_notification.sh
+++ src/scripts/spam_notification.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 phrase[0]="You're not a complete idiot. Some pieces are missing."
 phrase[1]="One man’s crappy software is another man’s full time job."
-phrase[2]="System Shutdown"
-phrase[3]="Deleting root"
+phrase[2]="System is shutting down... Please wait."
+phrase[3]="Deleting root..."
 phrase[4]="You've been pawned!"
 phrase[5]="Never trust a computer you can’t throw out a window."
-phrase[6]="You're not a complete idiot. Some pieces are missing."
-phrase[7]="Have you ever wondered why you can’t taste your tongue?"
+phrase[6]="Why you're so dumb? 😥"
+phrase[7]="Have you ever wondered why you can’t taste your tongue? 🤔"
 phrase[8]="I never apologise. I’m sorry, that’s just the way I am."
-phrase[9]="You laugh because imm different,i laugh because I just farted.."
+phrase[9]="You laugh because I'm different, I laugh because I just farted... 😏"
 phraser=${phrase[$RANDOM % ${#phrase[@]} ]}
 notify-send -i face-wink "$phraser"
